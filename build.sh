fe78a1e6df034eda8fc493ca60b5e4d578d40f77
Kike <bitbucket@lemambo.com>
2019-07-31 00:42:33 +0200

    + added special logging for build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+function log() {
+    printf "+++++++++++ BUILD.SH ++++++++++\n"
+    printf "+++ %s \n" $1
+    printf "+++++++++++++++++++++++++++++++\n"
+}
+
 . toolchain/build_settings.conf
 
 export APP_NAME="TortoiseHg"
@@ -11,41 +17,56 @@ rm -rf dist/TortoiseHg.app
 mkdir -p ${DOWNLOADDIR}
 
 # build/verify dependencies
+log openssl.sh
 sh toolchain/receipts/openssl.sh
+log python.sh
 sh toolchain/receipts/python.sh
+log pip.sh
 sh toolchain/receipts/pip.sh
 
 if [ ${QT_VERSION} = "qt5" ]; then
+  log qt5.sh
   sh toolchain/receipts/qt5.sh
 else
+  log qt4.sh
   sh toolchain/receipts/qt4.sh
 fi
+log qscintilla.sh
 sh toolchain/receipts/qscintilla.sh
+log sip.sh
 sh toolchain/receipts/sip.sh
 if [ ${QT_VERSION} = "qt5" ]; then
-  sh toolchain/receipts/pyqt5.sh
+    log pyqt5.sh
+    sh toolchain/receipts/pyqt5.sh
 else
-  sh toolchain/receipts/pyqt4.sh
+    log pyq4.sh
+    sh toolchain/receipts/pyqt4.sh
 fi
 
+log qscintilla.sh
 sh toolchain/receipts/qscintilla.sh
+log packages.sh
 sh toolchain/receipts/packages.sh
 
 # build mercurial + tortoisehg
+log mercurial.sh
 sh toolchain/receipts/mercurial.sh
+log tortoisehg.sh
 sh toolchain/receipts/tortoisehg.sh
 
 # create application package
-. toolchain/build_settings.conf
+log "application package"
+
 python setup.py
 
 if [ -d dist/${APP_NAME}.app ]; then
   rm -rf build
   rm -rf toolchain/build
 
-  if [ ${QT_VERSION} = "qt5" ]; then
+  if [ "${QT_VERSION}" = "qt5" ]; then
     macdeployqt dist/${APP_NAME}.app -always-overwrite
     cp -R ${DISTDIR}/usr/lib/QtNetwork.framework dist/${APP_NAME}.app/Contents/Frameworks/
   fi
+  log "createDmg.sh"
   sh toolchain/receipts/createDmg.sh
 fi
