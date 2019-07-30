a6a6b334c3827696090a6a1d0049160a8da04372
Kike <bitbucket@lemambo.com>
2019-07-31 00:41:52 +0200

    + improved tortoisehg script

--- toolchain/receipts/tortoisehg.sh
+++ toolchain/receipts/tortoisehg.sh
@@ -2,10 +2,37 @@
 
 . toolchain/build_settings.conf
 
-cd src/thg
+NAME="tortoisehg"
+VERSION="4.9.1"
+VERIFY_FILE="$BUILDDIR/${NAME}-${VERSION}/thg"
+DOWNLOAD_ADDR="https://bitbucket.org/tortoisehg/targz/downloads/${NAME}-${VERSION}.tar.gz"
+DOWNLOAD_FILE="${DOWNLOADDIR}/${NAME}-${VERSION}.tar.gz"
 
-python setup.py clean
-python setup.py build
-python setup.py install
+if [ ! -f $VERIFY_FILE ]; then
 
-cd ${ROOT_PATH}
+  if [ ! -f $DOWNLOAD_FILE ]; then
+    echo "Downloading ${DOWNLOAD_ADDR}"
+    curl -L $DOWNLOAD_ADDR --output ${DOWNLOAD_FILE}
+  fi
+  
+  rm -rf ${BUILDDIR}/${NAME}-${VERSION}
+  mkdir -p ${BUILDDIR}
+
+  if [ ! -d ${BUILDDIR}/${NAME}-${VERSION} ]; then
+    echo "Extracting ${DOWNLOAD_FILE}"
+    cd ${BUILDDIR}
+    tar -xf ${DOWNLOAD_FILE}
+    cd ${NAME}-${VERSION}
+  else
+    cd ${BUILDDIR}/${NAME}-${VERSION}
+  fi
+
+  python setup.py clean
+  python setup.py build
+  python setup.py install
+
+  cd $ROOT_DIR
+
+else
+  echo "${NAME} already installed."
+fi
