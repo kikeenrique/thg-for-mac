67f861c1288a1f373fdc580f3f57a30206788ba4
Kike <bitbucket@lemambo.com>
2019-08-02 23:58:22 +0200

    + remove bad and quick test code.

--- build.sh
+++ build.sh
@@ -14,12 +14,8 @@ export QT_VERSION="qt5"
 
 PRECOMPILED_FILE="${DISTDIR}.zip"
 if [ -f $PRECOMPILED_FILE ]; then
-    echo "guay"
     unzip ${PRECOMPILED_FILE} -d ${ROOT_DIR}/toolchain
-    exit
 fi
-
-unzip  -d
 
 rm -rf dist/TortoiseHg.app
 
