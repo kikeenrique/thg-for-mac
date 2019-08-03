f30d20ed5d9f0296156d316a17650fc84bc14631
Kike <bitbucket@lemambo.com>
2019-08-03 02:55:08 +0200

    + Fix dmgfiles location

--- toolchain/receipts/deployTo.sh
+++ toolchain/receipts/deployTo.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-DMGFILES=( *.dmg )
-
 # Deploy to bitrise
 mkdir deploy
 ls -la
-mv ${$DMGFILES}.dmg deploy/
+mv *.dmg deploy/
 
 # Deploy to bitbucket
-curl -X POST --user "${BB_AUTH_STRING}" "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"deploy/${DMGFILES}.dmg"
-
+DMGFILES=( deploy/*.dmg )
+echo $DMGFILES
+curl -X POST --user "${BB_AUTH_STRING}" "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"${DMGFILES}"
