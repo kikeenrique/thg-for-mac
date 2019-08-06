795d576b652bf368e658c36d8f6567e0170bde31
Kike <bitbucket@lemambo.com>
2019-08-07 01:50:12 +0200

    + reinstall pip always

--- toolchain/receipts/pip.sh
+++ toolchain/receipts/pip.sh
@@ -7,7 +7,7 @@ VERIFY_FILE="$DISTDIR/System/Library/Frameworks/Python.framework/Versions/Curren
 DOWNLOAD_ADDR="http://bootstrap.pypa.io/get-pip.py"
 DOWNLOAD_FILE="${DOWNLOADDIR}/get-pip.py"
 
-if [ ! -f $VERIFY_FILE ]; then
+#if [ ! -f $VERIFY_FILE ]; then
 
   if [ ! -f $DOWNLOAD_FILE ]; then
     echo "Downloading ${DOWNLOAD_ADDR}"
@@ -17,18 +17,18 @@ if [ ! -f $VERIFY_FILE ]; then
   python ${DOWNLOAD_FILE}
 
   cd $ROOT_DIR
-else
-  echo "${NAME} already installed."
-  export PATH="$DISTDIR/System/Library/Frameworks/Python.framework/Versions/Current/bin/":$PATH
+#else
+#  echo "${NAME} already installed."
+#  export PATH="$DISTDIR/System/Library/Frameworks/Python.framework/Versions/Current/bin/":$PATH
+#
+#  echo "modifying installed ${NAME}"
+#  cat ${VERIFY_FILE}
+#  # For pip, replace shebang with a generic execution, without customs paths
+##  sed -i '/#!/c\/usr/bin/env python' ${VERIFY_FILE}
+#  sed -i '' -e '1d' ${VERIFY_FILE}
+#  sed -i '' '1i\
+##!/usr/bin/env python
+#'  ${VERIFY_FILE}
+#  cat ${VERIFY_FILE}
 
-  echo "modifying installed ${NAME}"
-  cat ${VERIFY_FILE}
-  # For pip, replace shebang with a generic execution, without customs paths
-#  sed -i '/#!/c\/usr/bin/env python' ${VERIFY_FILE}
-  sed -i '' -e '1d' ${VERIFY_FILE}
-  sed -i '' '1i\
-#!/usr/bin/env python
-'  ${VERIFY_FILE}
-  cat ${VERIFY_FILE}
-
-fi
+#fi
